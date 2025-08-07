a60e803865c614aae659b031f81a30698a1668e8
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-08-07 16:51:44 +0100

    NoteList-use-client

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import css from "./NoteList.module.css";
-import NoteListClient from "./NoteListClient";
 import Link from "next/link";
 import type { Note } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../lib/api";
 
 export interface NoteListProps {
   notes: Note[];
@@ -11,6 +14,15 @@ export default function NoteList({ notes }: NoteListProps) {
   if (!notes || notes.length === 0) {
     return null;
   }
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (noteId: string) => deleteNote(noteId),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
 
   return (
     <ul className={css.list}>
@@ -23,7 +35,12 @@ export default function NoteList({ notes }: NoteListProps) {
             <Link className={css.link} href={`/notes/${note.id}`}>
               View details
             </Link>
-            <NoteListClient noteId={note.id} />
+            <button
+              className={css.button}
+              onClick={() => mutation.mutate(note.id)}
+            >
+              Delete
+            </button>
           </div>
         </li>
       ))}
